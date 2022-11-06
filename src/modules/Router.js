ecb8e840046177d75e60bc1dc9eafd26af71c46b
Arseniy Romanovskiy <ronyplay247@gmail.com>
2022-11-06 21:50:14 +0300

    Added router menu

---
+++ src/modules/Router.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+export default class Router extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = 
+            {
+                menus: 
+                    [
+                        {
+                            name: 'Home',
+                            icon: [<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>]
+                        },
+                        {
+                            name: 'Patterns',
+                            icon: [<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>]
+                        },
+                        {
+                            name: 'Learn',
+                            icon: [<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 14l9-5-9-5-9 5 9 5z"></path><path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path></svg>]
+                        },
+                        {
+                            name: 'About',
+                            icon: [<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>]
+                        },
+                        {
+                            name: 'Support',
+                            icon: [<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>]
+                        }
+                    ]
+            }
+    }
+
+    getMenu() {
+        return [
+            <div className="w-11/12 mt-3 flex flex-col">
+            {
+                this.state.menus.map((el) => {
+                    return [<div className="router-item flex flex-row items-center">{el.icon}{el.name}</div>]        
+                })
+            }
+            </div>
+            ];
+    }
+
+    render() {
+        return (
+            <div className="router flex flex-col items-center w-1/5">
+                <h1 className="router-name">conway's lifegame</h1> 
+                {this.getMenu()}
+            </div>
+        )
+    }
+}
