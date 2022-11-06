d2c07e2821a0b13e6d0c00d75481f895830b7801
Arseniy Romanovskiy <ronyplay247@gmail.com>
2022-11-06 20:54:54 +0300

    Remove trash

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    //screens: {
-    //    'xs': '320px'
-    //},
     extend: {},
   },
   plugins: [
-    require('tw-elements/dist/plugin'),
     require("@tailwindcss/forms")
   ],
 }
